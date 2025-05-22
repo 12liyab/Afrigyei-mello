862b30bfb887f81c431e14c5161930e9131bfe5d
Homebwoy18 <fawuzanmashudu18@gmail.com>
2025-05-22 04:01:52 -1200

    worked on json

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const crypto = require('crypto');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
